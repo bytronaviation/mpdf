40157475b313e170438bf9507192ceaba6bfeae9
bytronaviation <51328563+bytronaviation@users.noreply.github.com>
2020-11-30 15:13:51 +0000

    fix issue where unset index was being used

--- src/Writer/BaseWriter.php
+++ src/Writer/BaseWriter.php
@@ -202,7 +202,11 @@ final class BaseWriter
 
 				$h = ($m[1] / Mpdf::SCALE);
 				// Update/overwrite the lowest bottom of printing y value for a column
-				$this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'] = max($this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'], $this->mpdf->y + $h);
+				if(array_key_exists($this->mpdf->CurrCol, $this->mpdf->ColDetails)) {
+					$this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'] = max($this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'], $this->mpdf->y + $h);
+				} else {
+					$this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'] = $this->mpdf->y + $h;
+				}
 
 			} elseif (isset($this->mpdf->ColDetails[$this->mpdf->CurrCol]['bottom_margin'])) {
 
